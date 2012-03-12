e201823b655e1cae8863741b91776d9a59840027
Alberto Avila <albertoavila@gmail.com>
2012-03-11 20:35:14 -0700

    Deleted uneeded build files

--- make-w.sh
+++
@@ -1,5 +0,0 @@
-BASEPATH=c:\\xampp\\htdocs\\wp\\wp-content\\themes\\coltsintheme
-if [ ! -d "$BASEPATH" ]; then
-    mkdir $BASEPATH;
-fi
-cp -fR src/* $BASEPATH; 
--- make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-BASEPATH=/var/www/wp/wp-content/coltsintheme
-if [ ! -d "$BASEPATH" ]; then
-    mkdir $BASEPATH;
-fi
-cp -fR src/* $BASEPATH; 
