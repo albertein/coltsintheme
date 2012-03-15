cb9ae76066bcdf93f6c89f237a35aa7e7b6ace42
Alberto Avila <albertoavila@gmail.com>
2012-03-15 14:14:24 -0700

    Minor improvements on /blog

--- src/blog.php
+++ src/blog.php
@@ -1,7 +1,50 @@
 <?
 global $coltsin_mustache;
 $template = coltsin_get_template('blog');
-$data = array(
-	      );
+$posts = array();
+$index = 0;
+$traslated_months = array(
+			  'Jan' => 'Ene',
+			  'Feb' => 'Feb',
+			  'Mar' => 'Mar',
+			  'Apr' => 'Abr',
+			  'May' => 'May',
+			  'Jun' => 'Jun',
+			  'Jul' => 'Jul',
+			  'Aug' => 'Ago',
+			  'Sep' => 'Sep',
+			  'Oct' => 'Oct',
+			  'Nov' => 'Nov',
+			  'Dec' => 'Dec');
+query_posts('posts_per_page=5');
+if (have_posts()) {
+  while(have_posts()) {
+    the_post();
+    $tags = array();
+    $tags_raw = get_the_tags();
+    $count = 0;
+    if (is_array($tags_raw)) {
+      foreach($tags_raw as $tag) {
+	$tags[$count] = array(
+			      'name' => $tag->name,
+			      'tag_link' => get_tag_link($tag->term_id)
+			      );
+	$count++;
+      }
+    }
+	
+    $posts[$index] = array(
+			    'title' => get_the_title(),
+			    'permalink' => get_permalink(get_the_ID()),
+			    'content' => get_the_content(),
+			    'day' => get_the_date('j'),
+			    'month' => get_the_date('M'),
+			    'tags' => $tags
+			    );
+    $index++;
+
+  }
+}
+$data = array('posts' => $posts);
 echo $coltsin_mustache->render($template, $data);
 ?>
--- src/footer.php
+++ src/footer.php
@@ -5,7 +5,10 @@
         wp_footer();
     });
     $data = array(
-        'wp-footer' => $wp_footer
+		  'wp-footer' => $wp_footer,
+		  'theme-path' => get_bloginfo('template_url'),
+		  'is_front_page' => is_front_page(),
+		  'home-url' => get_home_url()
     );
     echo $coltsin_mustache->render($template, $data);
 ?>
