f5a75e485304524dccb712c477e3ba266ce71381
Alberto Avila <albertoavila@gmail.com>
2012-03-21 10:51:26 -0700

    Fixed bug on front-page, now it only displays the last event

--- src/front-page.php
+++ src/front-page.php
@@ -7,7 +7,7 @@ $events = array();
 $index = 0;
 query_posts(array(
 		  'cat' => $options['events_category'],
-		  'posts_per_page=1'));
+		  'posts_per_page' => 1));
 if (have_posts) {
   while(have_posts()) {
     the_post();
