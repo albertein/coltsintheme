41b86ac45f13381dfd8580daa2d0cae001f296ec
Alberto Avila <albertoavila@gmail.com>
2012-03-21 18:58:22 -0700

    Fixed front_page and sidebar bugs
    front_page was modifying the loop while executing query_posts making is_front_page return false on fotter, fixed it by changing query_posts to get_posts method.
    
    sidebar left margin was too big causing the main content to be displaced downward on some browsers

--- src/front-page.php
+++ src/front-page.php
@@ -5,23 +5,20 @@ $template = coltsin_get_template('front-page');
 $options = get_option('coltsin_display_options');
 $events = array();
 $index = 0;
-query_posts(array(
+$items = get_posts(array(
 		  'cat' => $options['events_category'],
-		  'posts_per_page' => 1));
-if (have_posts) {
-  while(have_posts()) {
-    the_post();
-    $event_date = new DateTime(get_post_meta(get_the_ID(), 'eventdate', true));
-    $now = new DateTime(date('Y-m-d'));
-    $eta = $now->diff($event_date)->format('%a');
-    $events[$index] = array(
-			    'title' => get_the_title(),
-			    'content' => get_the_content(),
-			    'permalink' => get_permalink(get_the_ID()),
-			    'ETA' => $eta
-			    );
-    $index++;
-  }
+		  'number_posts' => 1));
+foreach($items as $item) {
+  $event_date = new DateTime(get_post_meta($item->ID, 'eventdate', true));
+  $now = new DateTime(date('Y-m-d'));
+  $eta = $now->diff($event_date)->format('%a');
+  $events[$index++] = array(
+			  'title' => $item->post_title,
+			  'content' => $item->post_content,
+			  'permalink' => get_permalink($item->ID),
+			  'ETA' => $eta
+			  );
+
 }
 $data = array('events' => $events);
 echo $coltsin_mustache->render($template, $data);
